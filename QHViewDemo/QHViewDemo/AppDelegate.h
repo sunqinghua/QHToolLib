0c182b3bb037f85594843e90c0a1504a4969ea63
sunqinghua <453433112@qq.com>
2017-06-24 10:55:23 +0800

    提交内容

---
+++ QHViewDemo/QHViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QHViewDemo
+//
+//  Created by admin on 17/6/24.
+//  Copyright © 2017年 qinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QHViewDemo/QHViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QHViewDemo
+//
+//  Created by admin on 17/6/24.
+//  Copyright © 2017年 qinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
